864ae6f225463b1ad45c531293f6eed07b0afa3f
Joel Sanfeliu <joelsanfeliu2@gmail.com>
2026-05-31 16:17:20 +0200

    Fix constraint weights; target Optuna entirely on GA Search Params (mut_prob, elitism, tour_size). Also bumped pop_size and n_gen

--- optuna_optimizer.py
+++ optuna_optimizer.py
@@ -4,24 +4,21 @@ from ga_optimizer import OptimizerContext, evolve, evaluate_schedule
 def run_optuna_study(ctx: OptimizerContext, n_trials: int = 50, seed: int = 42):
     def objective(trial: optuna.Trial) -> float:
         # Fixed hyperparameters to avoid excessive computation times
-        pop_size = 40
-        n_gen = 60
+        pop_size = 60
+        n_gen = 100
         
         # 3 GA parameters to tune
         mut_prob = trial.suggest_float("mut_prob", 0.1, 1.0)
         elitism = trial.suggest_int("elitism", 1, 10)
         tour_size = trial.suggest_int("tour_size", 2, 7)
         
-        # 3 Penalty weights to tune
-        w_cap = trial.suggest_float("w_cap", 10.0, 500.0, log=True)
-        w_inc = trial.suggest_float("w_inc", 1000.0, 20000.0, log=True)
-        w_urg = trial.suggest_float("w_urg", 100.0, 1000.0, log=True)
+        # Fixed Penalty weights
+        ctx.w_cap = 100.0
+        ctx.w_inc = 10000.0
+        ctx.w_urg = 500.0
 
         ctx.mut_prob = mut_prob
         ctx.tour_size = tour_size
-        ctx.w_cap = w_cap
-        ctx.w_inc = w_inc
-        ctx.w_urg = w_urg
 
         best_ind, _ = evolve(
             ctx,
